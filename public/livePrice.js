6fa384dfcafd60b7e8283d883ba932a1c2668217
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-22 10:51:03 +0530

    added fn to display live price on frontend

---
+++ public/livePrice.js
@@ -0,0 +1,15 @@
+const eventSource = new EventSource('http://localhost:8000/api/liveprice')
+
+const priceDisplay = document.getElementById('price-display');
+
+// HANDLE LIVE PRICE UPDATe
+eventSource.onmessage = (event) => {
+    const data = JSON.parse(event.data)
+    const price = data.price
+    priceDisplay.textContent = price
+}
+
+// HANDLE CONNECTION LOSS
+eventSource.onerror = () => {
+    console.log('Connection Lost. Attempting to reconnect...')
+}
